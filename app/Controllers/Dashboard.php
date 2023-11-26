bd56adb18711c4e5bf3d9dd38381fe27763ee6ef
hendrawan-19220059 <19220059@bsi.ac.id>
2023-11-26 21:25:16 +0700

    Tambah controller Dashboard & setting BaseController

---
+++ app/Controllers/Dashboard.php
@@ -0,0 +1,19 @@
+<?php
+ 
+namespace App\Controllers;
+ 
+use App\Controllers\BaseController;
+ 
+class Dashboard extends BaseController
+{
+    public function index()
+    {
+        session();
+        $data = [
+            'title' => "Dashboard". $this->judul_web,
+        ];
+
+        return view('templates/header', $data) . view('dashboard') . view('templates/footer');
+
+    }
+}
